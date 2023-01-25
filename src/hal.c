691cc485f583f755cd6491fae849faba0c4b393c
Murphy <smurphy@allegion.com>
2023-01-25 11:53:20 -0500

    Fixed comment in hal.c

--- src/hal.c
+++ src/hal.c
@@ -5,8 +5,8 @@
 
 int hal_reg_access(int arg1, int arg2) {
     // This could be a register access, etc.. Something that needs to be mocked away
-    // when running in unit tests. (If you remove the mock and call myFunction in
-    // the test case -  it will seg-fault for an unsafe memory access)
+    // when running in unit tests. (If you remove the mock and call hal_reg_access in
+    // the test case - it will seg-fault for an unsafe memory access)
     memcpy(arg1, arg2, sizeof(int));
 
     return 0;
